48eb3d382ed36404c25f0606c932faa2d797598b
Zach Buchar DESKTOP <kkzabuchar@gmail.com>
2022-04-07 11:52:54 -0500

    re enabled english ai

--- script.js
+++ script.js
@@ -91,8 +91,7 @@ xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
-    //    response.innerHTML = xhr.responseText;
-    response.innerHTML = "Due to usage limits of the OpenAI platform, this service is not longer operational.";
+       response.innerHTML = xhr.responseText;
    }};
 
 var data = "prompt=" + prompt;
